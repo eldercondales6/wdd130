de7a186bd499b784bb8cb9a70381c8aaa4c4855a
eldercondales6 <ac2556@buyi.edu>
2023-09-24 02:49:05 -0400

    Added last Update Site-Plan

---
+++ AssignW02.py
@@ -0,0 +1,31 @@
+print('Be welcome to this Funny Game!')
+print('Enter the Following down below to start playing:')
+adjective = 'happy'
+animal = 'Lion'
+verb1 = 'roar'
+exclamation = 'oops!'
+verb2 = 'run'
+verb3 = 'drive'
+print('adjective:')
+adjective = input()
+print('Animal:')
+animal = input()
+print('first verb:')
+verb1 = input()
+print('exclamation:')
+exclamation = input()
+print('second verb - it must be different than the before:')
+verb2 = input()
+print('third verb - it must be different than the two before:')
+verb3 = input()
+print('Your story is:')
+print('The other day, I was really in trouble. It all started when I saw a very' ' ' + adjective + ' ' + animal + ' ' 'yell down the hallway.' ' ' + exclamation + ' '  'I yelled. But I could think to do was to' ' ' + verb2 +  ' ' 'over and over. Miraculously, that caused it to stop, but not before it tried to' ' ' + verb3 + ' ' 'right in front of my family.')
+print('Thanks for participating in this excellent game.')
+
+
+
+
+
+
+
+
